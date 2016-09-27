1cad070810e9efecac63199c84baa7f0573abc5e
Clint Worrell <github@clintworrell.com>
2016-09-27 14:26:43 -0700

    Add Spotify search and current track functions

--- public/js/services/playlistFactory.js
+++ public/js/services/playlistFactory.js
@@ -2,12 +2,12 @@ app.factory('playlistFactory', function($http) {
     var playlist = [];
     var factory = {};
 
-    factory.testPlaylist = function () {
-      playlist.push("Test1");
-      playlist.push("Test2");
-      playlist.push("Test3");
-      return playlist;
-    };
+    // factory.testPlaylist = function () {
+    //   playlist.push("Test1");
+    //   playlist.push("Test2");
+    //   playlist.push("Test3");
+    //   return playlist;
+    // };
 
     factory.getPlaylists = function (callback) {
       $http.get('/api/playlists').success(callback);
@@ -15,6 +15,15 @@ app.factory('playlistFactory', function($http) {
 
     factory.getPlaylist = function (playlistId, callback) {
       $http.get(`/api/playlist/${playlistId}`).success(callback);
+    };
+
+    factory.spotifySearch = function(searchQuery, callback) {
+      $http.get(`/api/spotify_search/${searchQuery}`).success(callback);
+      // console.log("WHY AM I BEING CALLED?");
+    };
+
+    factory.currentTrack = function(callback) {
+      $http.get('/api/current_track').success(callback);
     };
 
     return factory;

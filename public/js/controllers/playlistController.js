75dbf26ea3cc9afa87cd1f4aa1f99d0294738f5f
Clint Worrell <github@clintworrell.com>
2016-09-27 14:25:01 -0700

    Add Spotify search and poller to refresh current track info

--- public/js/controllers/playlistController.js
+++ public/js/controllers/playlistController.js
@@ -1,7 +1,31 @@
-app.controller('PlaylistController', function($scope, $routeParams, playlistFactory) {
+app.controller('PlaylistController', function($scope, $routeParams, $http, $timeout, poller, playlistFactory) {
   $scope.view = {};
-  var playlistId = $routeParams.playlistId;
-  playlistFactory.getPlaylist(playlistId, function(data) {
+  $scope.view.playlistId = $routeParams.playlistId;
+  $scope.view.searchQuery = "";
+  $scope.view.currentTrack = "";
+  // $scope.view.showSearch = false;
+  // $scope.view.enableSearch = function() {
+  //   $scope.view.showSearch = !$scope.view.showSearch;
+  // };
+  playlistFactory.getPlaylist($scope.view.playlistId, function(data) {
     $scope.view.playlist = data.items;
   });
+
+  $scope.view.spotifySearch = function() {
+    playlistFactory.spotifySearch($scope.view.searchQuery, function(data) {
+      $scope.view.searchResults = data;
+    });
+  };
+
+  var currentTrackPoller = poller.get('/api/current_track', {
+    delay: 2000
+  });
+
+  currentTrackPoller.promise.then(null, null, function(data) {
+    $scope.view.currentTrack = data.data;
+  });
+  // playlistFactory.currentTrack(function(data) {
+  //   $scope.view.currentTrack = data;
+  // });
+
 });
